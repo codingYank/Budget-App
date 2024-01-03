b9e73aef35c6be1bde4d02ede38440a8ceaff7ac
codingYank <tyler.schmidt99@gmail.com>
2024-01-03 13:35:42 -0500

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+import connectDB from "./config/db.js"
+const port = process.env.PORT || 5000
+
+connectDB()
+
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("API is running")
+})
+
+app.listen(port, () => console.log(`Server is running on port ${port}`))
