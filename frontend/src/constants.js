bfcd345fb1ce28fce136d20611b2fe0409fe270a
codingYank <tyler.schmidt99@gmail.com>
2024-01-15 15:37:22 -0500

    redux and routes

---
+++ frontend/src/constants.js
@@ -0,0 +1,5 @@
+export const BASE_URL =
+  process.env.NODE_ENV === "development" ? "http://localhost:5000" : ""
+export const USERS_URL = "/api/users"
+export const TRANSACTIONS_URL = "/api/transactions"
+export const CATEGORIES_URL = "/api/categories"
