1809c576187f9fcdc910e22df24621d11af27b6d
codingYank <tyler.schmidt99@gmail.com>
2024-02-01 21:59:17 -0500

    adding paycheck

--- frontend/src/components/AddPaycheck.jsx
+++ frontend/src/components/AddPaycheck.jsx
@@ -1,7 +1,8 @@
-import { Form, Formik } from 'formik'
-import React from 'react'
+import { Field, FieldArray, Form, Formik } from 'formik'
+import React, { useState } from 'react'
 
 const AddPaycheck = ({ show, refetchCategories, refetchUser, categories }) => {
+  const [uncategorized, setUncategorized] = useState(0)
   const close = () => {
     show(false)
   }
@@ -17,24 +18,56 @@ const AddPaycheck = ({ show, refetchCategories, refetchUser, categories }) => {
   const initValues = {
     name: '',
     value: 0,
-    categoryIds
+    categories: categoryIds
   }
 
   const onSubmit = (e) => {
     console.log(e)
   }
 
-  console.log(initValues)
+  const onChange = (e) => {
+    let array = []
+    for (let i = 0; i <= categoryIds.length + 1; i++) {
+      array.push(e.target.form[i].value)
+    }
+    let paycheck = Number(array[1])
+    let sum = 0
+    for (let i = 2; i < array.length; i++) {
+      sum += Number(array[i])
+    }
+    setUncategorized(paycheck - sum)
+  }
+
   return (
     <div className='modal-screen'>
       <div className='modal-content'>
         <button type='button' onClick={close} className='invisable-btn'>X</button>
         <h1>New Paycheck</h1>
-        <Formik>
-          <Form>
+        <p>{uncategorized}</p>
+        <Formik initialValues={initValues} onSubmit={onSubmit}>
+          <Form onChange={onChange}>
             <div className='form-content'>
-              
+              <label htmlFor='name'>Name</label>
+              <Field name='name' id='name' />
             </div>
+            <div className='form-content'>
+              <label htmlFor='value'>Paycheck Total</label>
+              <Field name='value' id='value' type='number'/>
+            </div>
+            <FieldArray name='categories'>
+              {() => (
+                <>
+                {initValues.categories.length > 0 && 
+                  initValues.categories.map((category, index) => (
+                    <div className='form-content' key={category.id}>
+                      <label htmlFor={category.id}>{category.name}</label>
+                      <Field name={`categories.${index}.depositAmount`} id={category.id} type='number' />
+                    </div>
+                  ))}
+                </>
+              )}
+            </FieldArray>
+            <button className='primary-btn' type='submit' >Create</button>
           </Form>
         </Formik>
       </div>
